af9e06935fffdc5a6b9f992b14b2871a4b0c208b
Roxanne Alvidrez Niederkorn <rox.niederkorn@gmail.com>
2022-11-23 11:50:22 -0600

    added id #button to html, emptied input values upon button click

--- script.js
+++ script.js
@@ -1,6 +1,50 @@
 console.log('The DOM is ready');
 $(readyNow);
 
+let employee = [];
+
+
 function readyNow(){
-    console.log('HIIII ')
-} // end onReady
+    //console.log('HIIII ');
+    $('.SubmitBtn').on('click', addNewEmployee);
+    //$('#button').on('click', appendToDom);
+   
+} // end onRead
+
+function addNewEmployee(){
+console.log('Adding Employee!');
+
+let newPerson = {
+    firstName: $('#first-name').val(),
+    lastName: $('#last-name').val(),
+    ID: $('#ID').val(),
+    title: $('#title').val(),
+    annualSalary: $('#annual-salary').val()
+}
+
+employee.push(newPerson);
+
+appendToDom();
+} // end addEmployee
+
+addNewEmployee();   // calling addNewEmployee function
+
+function appendToDom(){
+    console.log('Working!!!');
+    //console.log($(this));
+   // $('.employee-data').empty();
+    
+    for(person of employee){
+        $('.employee-data').empty().append(`
+        <td>${person.firstName}</td> <td>${person.lastName}</td>
+         <td>${person.ID}</td> <td>${person.title}</td> <td>${person.annualSalary}</td>
+        `);
+    }
+        $('#first-name').val(''),
+        $('#last-name').val(''),
+        $('#ID').val(''),
+        $('#title').val(''),
+        $('#annual-salary').val('')
+    } // end appendToDom function
+
+
